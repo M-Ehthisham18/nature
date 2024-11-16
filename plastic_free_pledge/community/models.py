52f2b305ed61c6fa913777341fbe4dbd1eb107eb
M-Ehthisham18 <ehthishamulhaq18@gamil.com>
2024-11-16 13:31:55 +0530

    models file updated :

--- plastic_free_pledge/community/models.py
+++ plastic_free_pledge/community/models.py
@@ -1,17 +1,30 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# class Pledge(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     date = models.DateField(auto_now_add=True)
+#     description = models.TextField()
+
+# class Challenge(models.Model):
+#     title = models.CharField(max_length=100)
+#     description = models.TextField()
+#     points = models.IntegerField(default=10)
+
+# class Achievement(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     date_earned = models.DateField(auto_now_add=True)
+
+
 from django.db import models
 from django.contrib.auth.models import User
 
 class Pledge(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
     description = models.TextField()
+    image = models.ImageField(upload_to='pledge_images/', null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)  # Automatically set the date when created
 
-class Challenge(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    points = models.IntegerField(default=10)
-
-class Achievement(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    date_earned = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.description
